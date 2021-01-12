b0d85d6486aed9f94112610b87cd273d1d73baf6
developert1990 <developert.hong@gmail.com>
2021-01-12 16:14:08 -0700

    add comment for api base

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
+// server단 즉 노드 에서는 aws에 배포를 할때 컨테이너 환경변수 NODE_ENV를 설정을 해줫는데 리액트에서는 build를 하면 자동으로 프로덕션으로 인식을해서 이렇게만 작성해주면 알아서 npm start하면 development로, build하면 production으로 판단을 한다.
 export const API_BASE = process.env.NODE_ENV === 'development' ? 'http://localhost:9002' : `http://${process.env.REACT_APP_API_BASE}:9002`;
 
 // material-ui constant Slider
